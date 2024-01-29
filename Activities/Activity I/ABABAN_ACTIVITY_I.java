5b9b71553e0b5131ca09f45cf8fe2c5ecd2f05d2
Kyle Marion N. Ababan <158006248+kyleababan@users.noreply.github.com>
2024-01-29 10:55:27 +0800

    Add files via upload

---
+++ Activities/Activity I/ABABAN_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+public class ABABAN_ACTIVITY_I{
+    public static void main(String []args){
+
+        int a = 10, b = 10, c = 5;
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("Product: "+prod);
+        System.out.println("Quotient: "+quot);
+        
+        
+
+
+    }
+}
+
+    
+
+    
