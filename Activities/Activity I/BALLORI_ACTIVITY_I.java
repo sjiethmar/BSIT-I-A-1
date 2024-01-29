3f9afc907cce193dde6422e8cfac9022748aad74
VanCyrilBallori <158006267+VanCyrilBallori@users.noreply.github.com>
2024-01-29 11:07:20 +0800

    Add files via upload

---
+++ Activities/Activity I/BALLORI_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+public class BALLORI_ACTIVITY_I{
+    public static void main(String []args){
+
+        int a = 10, b = 10, c = 5;
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Difference: "+diff);
+        System.out.println("Product: "+prod);
+        System.out.println("Quotient: "+quot);
+        
+        
+
+
+    }
+}
+
+    
