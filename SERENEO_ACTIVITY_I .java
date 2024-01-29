e354eefd4754c7caf652c9d32eaa715a66ffd11a
Sereneo Jiethmar C <158004974+sjiethmar@users.noreply.github.com>
2024-01-29 10:57:18 +0800

    Add files via upload

---
+++ SERENEO_ACTIVITY_I .java
@@ -0,0 +1,16 @@
+public class SERENEO_ACTIVITY_I{
+     public static void main (String[]args){
+       int a = 15;
+       int b = 4;
+       int c = 3;
+        int sum = a + b;
+        int difference = b - c;
+        int product = a * b;
+        int qoutient = c / a;
+
+        System.out.println("Sum" + sum);
+        System.out.println("Difference" - diff);
+        System.out.println("Product" * prod);
+        System.out.println("Quotient" quot);
+    }
+}
