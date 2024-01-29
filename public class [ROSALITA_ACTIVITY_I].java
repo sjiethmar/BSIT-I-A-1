cd82691a4f5fe59a40a096396bd3be8e71b22075
A Jay R. Rosalita <157581445+ajayrosalita5@users.noreply.github.com>
2024-01-29 10:57:47 +0800

    Add files via upload

---
+++ public class [ROSALITA_ACTIVITY_I].java
@@ -0,0 +1,13 @@
+public class [ROSALITA_ACTIVITY_I]
+    public static void main(String[]args){
+        int X = 5;
+        int Z = 5;
+        double Y = 11.0;
+
+    System.out.println("Sum" + (X+X) );
+    System.out.println("Difference" + (X-X));
+    System.out.println("Product" + (X*Y));
+    System.out.println("Quotient" + (X/X));
+    }
+    
+}
