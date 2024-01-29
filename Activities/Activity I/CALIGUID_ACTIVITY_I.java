16947c511f0b0bc12610f12a4cf30cd86d8b3a58
John franz O. Caliguid <158005719+johnfrank7@users.noreply.github.com>
2024-01-29 11:01:59 +0800

    Add files via upload

---
+++ Activities/Activity I/CALIGUID_ACTIVITY_I.java
@@ -0,0 +1,25 @@
+public class CALIGUID_ACTIVITY_I{
+
+    public static void main(String[]args){
+
+        int a = 10;
+        int b = 55;
+        int c = 45;
+
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+                System.out.println("Sum: " + sum);
+                System.out.println("Difference: " + diff);
+                System.out.println("Product: " + prod);
+                System.out.println("Quotient: " + quot);
+
+
+
+
+
+    }
+
+}
