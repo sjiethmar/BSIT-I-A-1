755cd09a1c65517e7eb6278ef526cab5b5d47d5d
Crystal Jeanne Ortega <158002778+crystaljeanne@users.noreply.github.com>
2024-01-29 10:56:02 +0800

    Add files via upload

---
+++ Activities/Activity I/ORTEGA_ACTIVITY_I.java
@@ -0,0 +1,11 @@
+public class ORTEGA_ACTIVITY_I{
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int z = 2;
+        System.out.println("Sum = "+(x+y+z));
+        System.out.println("Difference = "+(x-y-z));
+        System.out.println("Product = "+(x*y*z));
+        System.out.println("Quotient = "+(x/y/z));
+    }
+}
