61099bd423ac838271803ea0cbb76dca3d4ea355
Johan Carl Demotor <157077716+johancarldemotor@users.noreply.github.com>
2024-01-29 10:57:12 +0800

    Add files via upload

---
+++ Activities/Activity I/DEMOTOR_ACTIVITY_JAVA.java
@@ -0,0 +1,16 @@
+public class DEMOTOR_ACTIVITY_1
+
+public static void main(string[] args) {
+    int x = 15;
+    int y = 10;
+    int z = 5;
+
+    int sum = x + y;
+    int difference = x - y;
+    int product = x * y;
+    int quotient = x / z;
+
+    System.out.print(s:"sum");
+    System.out.print(s:"difference");
+    System.out.print("")
+
