7c6177952ccf385aa66b2d4f5b650891bb3830bd
Jenny Luo Cano Hiyan <158005724+jennyluo3@users.noreply.github.com>
2024-01-29 11:02:13 +0800

    Add files via upload

---
+++ Activities/Activity I/HIYAN_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+public class HIYAN_ACTIVITY_I{
+
+    public static void main(String[]args){
+
+        int a = 80;
+        int b = 9;
+        int c = 4;
+
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+                System.out.println("Sum: " + sum);
+                System.out.println("Difference: " + diff);
+                System.out.println("Product: " + prod);
+                System.out.println("Quotient: " + quot);
+
+
+
+    }
+}
