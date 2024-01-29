7ce6785cb99b5f6c96fa3f8d9615ee0fdb3ec76c
Sarcon Chane B <158005300+chanesarcon@users.noreply.github.com>
2024-01-29 11:03:20 +0800

    Add files via upload

---
+++ SARCON_ACTIVITY_1].java
@@ -0,0 +1,18 @@
+public class SARCON_ACTIVITY_1] {
+   public static void main(String []args){
+    int a = 10;
+    int b = 5;
+    int c = 2; 
+
+    int sum = a + b + c;
+    int difference = a - b - c;
+    int product = a * b * c;
+    int qoutient = a / b / c;
+     
+    System.out.println("Sum: " + sum);  
+    System.out.println("Difference: " - diff);
+    System.out.println("Product: " * prod);
+    System.out.println("Qoutient: " + qout); 
+    
+   }    
+}
