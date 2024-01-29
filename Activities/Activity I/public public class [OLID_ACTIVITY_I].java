c7abf0d234ce2f77f4af3850bb6823a293f2e867
kim louie N. olid <158005486+kimkim2003@users.noreply.github.com>
2024-01-29 11:03:42 +0800

    Add files via upload

---
+++ Activities/Activity I/public public class [OLID_ACTIVITY_I].java
@@ -0,0 +1,16 @@
+public class [OLID_ACTIVITY_I]
+    public static void main(String[]args){
+        int X = 5;
+        int X = 5;
+        double Y = 11.0;
+
+    System.out.println("Sum" + (X+X) );
+    System.out.ptintln("Difference" + (X+X));
+    System.out.println("product" + (X*Y));
+    System.out.println("Division"(X/Y));
+    }
+
+}
+ 
+
+
