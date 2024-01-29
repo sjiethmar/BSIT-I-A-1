ff29b71651de72955d62655f2d17dd7945664db8
John Lez Tayona <156988440+johntayona@users.noreply.github.com>
2024-01-29 10:57:37 +0800

    Add files via upload

---
+++ public class [TAYONA_ACTIVITY_1].java
@@ -0,0 +1,11 @@
+public class {TAYONA_ACTIVITY_1}
+    static void main (String []args){
+        int x = 5
+        int y = 10
+        int z = 15
+
+        System.out.println("Sum" + (x+y) );
+        System.out.println("Difference" - (z-y));
+        System.out.println("Product" * (x*z));
+        System.out.println("Quottient" % (x%y));
+    }
