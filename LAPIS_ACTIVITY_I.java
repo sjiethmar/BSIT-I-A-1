ab5f9b084fcfcf52983a97b74c97df70d9985c51
Jaild Katte C. Lapis <158005981+jaildkatte@users.noreply.github.com>
2024-01-29 10:59:20 +0800

    Add files via upload

---
+++ LAPIS_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class LAPIS_ACTIVITY_I{
+    public static void main(String[]args){
+        int x = 15;
+        int y = 10;
+        int z = 5;
+
+        int sum = x + y + z;
+        int difference = x - y - z;
+        int product = x * y * z;
+        int quotient = x / y / z;
+
+        System.out.print("sum: " + SUM);
+        System.out.print("difference: " + DIFFERENCE);
+        System.out.print("product: " + PRODUCT);
+        System.out.print("quotient: " + QUOTIENT);
+    }
+
+}
