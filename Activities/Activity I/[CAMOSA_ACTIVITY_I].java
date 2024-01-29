96b0ee20f5da838af5e40703bea8bb121d68dbd9
Hazel Camosa <157287321+HazelCamosa13@users.noreply.github.com>
2024-01-29 10:57:37 +0800

    Add files via upload

---
+++ Activities/Activity I/[CAMOSA_ACTIVITY_I].java
@@ -0,0 +1,15 @@
+public class CAMOSA_ACTIVITY_I{ 
+ public class void main (String[]args){
+int x = 8;
+int y = 5;
+int z= 6;
+
+int sum = x + y + z;
+int diff = z - y - x;
+int prod = x * y * z;
+int quot = z / y / x;
+
+System.out.println(sum);
+System.out.println(difference);
+System.out.println(product);
+System.out.println(quotient);
