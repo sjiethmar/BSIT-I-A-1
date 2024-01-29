61b1e80d1dc4a831bab9f7867f5d87e9f16c512e
Gian Gadiano Villarmia <157211284+GianVillarmia18@users.noreply.github.com>
2024-01-29 11:17:42 +0800

    Add files via upload

---
+++ Activities/Activity I/VILLARMIA_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class VILLARMIA_ACTIVITY_I{    
+    public static void main(String []args){ 
+    int x = 10;
+    int y = 20;
+    int z = 30;
+    
+    int sum = x + y + z;
+    int diff = x - y - z;
+    int prod = x * y * z;
+    int quot = x / y / z;
+    
+    System.out.println("addition = "+sum);
+    System.out.println("subtraction = "+diff);
+    System.out.println("multiplication = "+prod);
+    System.out.println("division = "+quot);
+    
+    }
+    
+    }
+    
