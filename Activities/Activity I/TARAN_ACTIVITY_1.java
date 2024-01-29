a33bbdd477878fae33d04b1cdfd4824d355852e8
Earl Joseph D. Taran <156991493+Earljoseph21@users.noreply.github.com>
2024-01-29 11:02:34 +0800

    Add files via upload

---
+++ Activities/Activity I/TARAN_ACTIVITY_1.java
@@ -0,0 +1,17 @@
+public class TARAN_ACTIVITY_1 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int c = 10;
+
+        int sum = a + b;
+        int difference = c - a;
+        int product = a * b;
+        int qoutient = c / a;
+
+        System.out.print(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+    }
+}
