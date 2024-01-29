9aceb48d45cd1e316c1ba84cd9a1cce2135a6f33
Elaine Nicole Ipili <157080275+ipilielainenicole@users.noreply.github.com>
2024-01-29 11:07:08 +0800

    Add files via upload

--- Activities/Activity I/IPILI_ACTIVITY_I.java
+++ Activities/Activity I/IPILI_ACTIVITY_I.java
@@ -1,11 +1,15 @@
 public class IPILI_ACTIVITY_I{
-    public static void main(String[]args){
-        int a = 12 , b = 3;
-        System.out.println("SUM= "+(a+b));
-        System.out.println("DIFFERENCE= "+(a-b));
-        System.out.println("PRODUCT =" +(a*b));
-        System.out.println("QUOTIENT =" +(a/b));
+    public static void main (String[]args){
+        int a = 5;
+        int b = 6;
+        int c = 4;
+        System.out.println("Sum= "+a+b);
+        System.out.println("Difference= "+(b-c) );
+        System.out.println("Product= "+(a*c) );
+        System.out.println("Quotient= "+(b/c));
 
 
     }
+
 }
+  
