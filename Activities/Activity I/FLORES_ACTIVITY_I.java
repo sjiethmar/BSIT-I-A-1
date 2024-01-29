99a28f9c0ed9fdf0131d2b1bc60379d89a393ad4
Eunice Gabrielle Flores <158002966+fncyz@users.noreply.github.com>
2024-01-29 10:56:52 +0800

    Add files via upload

---
+++ Activities/Activity I/FLORES_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+public class FLORES_ACTIVY_I{
+    public static void main (String []args){
+        int q = 10;
+        int w = 2;
+        int x = 15;
+        
+        int sum= q+w+x;
+        int diff= q-w;
+        int prod= w*q;
+        int quo= q/w;
+        System.out.println("Sum=" + sum);
+        System.out.println("Difference =" + diff);
+        System.out.println("Multiplication= " + prod);
+        System.out.println("Quotient= " + quo);
+    }
+}
