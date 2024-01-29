445bebefacce6eb68fc671fc0bc2216f145b82d9
Rhea Mae L. Jayme <157574770+rheajayme@users.noreply.github.com>
2024-01-29 11:00:13 +0800

    Add files via upload

---
+++ Activities/Activity I/JAYME_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+public class JAYME_ACTIVITY_1{ 
+  public static void main (String[]args){
+
+    int a = 5;
+    int b = 6;
+    int c = 4;
+
+    int sum = a + b + c;
+    int diff = a - b - c;
+    int prod = a * b * c;
+    int qout =a / b / c;
+
+    System.out.println(" Sum:"+sum);
+    System.out.println(" Diff:"+diff);
+    System.out.println("Prod:"+prod);
+    System.out.println("Qout:"+qout);
+
+  }
+}
