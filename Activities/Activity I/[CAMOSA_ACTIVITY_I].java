1d86712884c2263a521e01572fcc20f48aa919ea
Hazel Camosa <157287321+HazelCamosa13@users.noreply.github.com>
2024-01-29 11:20:10 +0800

    Update CAMOSA_ACTIVITY_I.java

--- Activities/Activity I/[CAMOSA_ACTIVITY_I].java
+++ Activities/Activity I/[CAMOSA_ACTIVITY_I].java
@@ -1,15 +1,18 @@
 public class CAMOSA_ACTIVITY_I{ 
- public class void main (String[]args){
+ public static void main(String[]args){
 int x = 48;
 int y = 15;
 int z= 26;
 
-int sum = x + y + z;
-int diff = z - y - x;
-int prod = x * y * z;
-int quot = z / y / x;
+int sum = x+y+z;
+int diff = z-y-x;
+int prod = x*y*z;
+int quot = z/y/x;
 
-System.out.println("Sum:" + sum);
-System.out.println("Difference:" - diff);
-System.out.println("Product:" * prod);
-System.out.println("Quotient:" / quot);
+System.out.println(x+y+z);
+System.out.println("Difference " + diff);
+System.out.println("Product " + prod);
+System.out.println("Quotient " + quot);
+
+   }
+}
