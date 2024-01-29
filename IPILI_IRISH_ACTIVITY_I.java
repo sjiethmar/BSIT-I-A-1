851063ebefd10854d7886614b6231c9d02b0e1e2
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-01-29 11:11:00 +0800

    Add files via upload

---
+++ IPILI_IRISH_ACTIVITY_I.java
@@ -0,0 +1,11 @@
+public class IPILI_IRISH_ACTIVITY_I{
+    public static void main(String[]args){
+        int a = 12 , b = 3;
+        System.out.println("SUM= "+(a+b));
+        System.out.println("DIFFERENCE= "+(a-b));
+        System.out.println("PRODUCT =" +(a*b));
+        System.out.println("QUOTIENT =" +(a/b));
+
+
+    }
+}
