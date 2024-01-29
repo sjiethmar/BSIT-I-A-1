dd6b0d11381126639761883fd354aba5f1c0f21f
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-01-29 10:59:23 +0800

    Add files via upload

---
+++ Activities/Activity I/BRIONES_Activity_1.java
@@ -0,0 +1,13 @@
+public class Briones_Activity_1{
+    public class void main(String []args){
+        int a = 2;
+        int b = 3;
+        int c = 4;
+
+        System.out.println("Sum "a+b);
+        System.out.println("Difference "c-b);
+        System.out.println("Product "a*c);
+        system.out.println("Qoutient "a/b);
+
+    }
+}
