15e2ed64af5fe4a381d840cc5029cb802c925265
Justeane faith alfornon <158006952+Justeane@users.noreply.github.com>
2024-01-29 11:01:53 +0800

    Add files via upload

---
+++ Activities/Activity I/ALFORNON_ACTIVITY I.java
@@ -0,0 +1,18 @@
+public class ALFORNON_ACTIVITY_1.java{
+    public class static void main(String args []){
+        int a = 15;
+        int b = 25;
+        int c = 30;
+        
+        int sum = a + b + c;
+        int difference = c - a;
+        int product = a * b;
+        int qoutient = c / a;
+
+        System.out.printIn(sum);
+        System.out.printIn(difference);
+        System.out.printIn(product);
+        System.out.printIn(qoutient);
+
+    }
+}
