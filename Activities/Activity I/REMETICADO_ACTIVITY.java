68035defbf4dc123c19e9a837b0f860a53896dec
Jedryl Remeticado <158005371+jeduuujed@users.noreply.github.com>
2024-01-29 11:00:35 +0800

    Add files via upload

---
+++ Activities/Activity I/REMETICADO_ACTIVITY.java
@@ -0,0 +1,17 @@
+
+public class REMETICADO_ACTIVITY{
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        int sum = x + y + z;
+        int diff = z - y - x;
+        int prod = x * y * z;
+        int qout = z / y / x;
+
+        System.out.println("Sum:" + sum);
+    }
+}
+
+    
