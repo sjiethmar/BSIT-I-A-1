dcc2cd72bc36e8638e30c664c09e4ba7233bd844
Joward Remeticado <157957512+Wardo30@users.noreply.github.com>
2024-01-29 11:02:15 +0800

    Add files via upload

---
+++ Activities/Activity I/REMETICADO_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class Main REMETICADO_ACTIVITY_I {
+
+puclic static void main(Sting[]args){
+    int x = 5 y =10, t = 15;
+
+    int sum = x+y+t;
+    int difference = x-y-t;
+    int product  = x*y*t;
+    int qoutient = x/y/t;
+    
+    System.out.println ("The Sum is " +(x+y+t));
+    System.out.println ("The Difference is "+(x-y-t));
+    System.out.println ("The Product is "+(x*y*t));
+    System.out.println ("The Quotient is"(x/y/t));
+
+
+}
+}
+    
+
