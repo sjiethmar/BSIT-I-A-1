f7196239f0d9fa971c9e023926e3186b0815dc11
KurtElemino <158981359+KurtElemino@users.noreply.github.com>
2024-02-05 10:46:58 +0800

    Create ELEMINO_ACTIVITY_II.java

---
+++ Activities/Activity II/ELEMINO_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ELEMINO_ACTIVITY_II{
+
+public static void main(String[]args)
+Scanner sc = new Scanner();
+
+int value;
+System.out.print(s:"Enter your first integer:");
+int num = sc.nextInt();
+System.out.print(s:"Enter your second integer:");
+int num2nd= sc.nextInt();
+
+System.out.print("Sum =" +(num + num2nd));
+System.out.print("Difference =" +(num - num2nd));
+System.out.print("Product =" +(num * num2nd));
+
+}
