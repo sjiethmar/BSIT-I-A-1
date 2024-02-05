9187bc4df64819992ed4af5c49d9f479e2838a8d
John Lloyd Camaso <151697948+redwind419@users.noreply.github.com>
2024-02-05 10:42:17 +0800

    Create CAMASO_ACTIVITY_II.java

---
+++ Activities/Activity II/CAMASO_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CAMASO_ACTIVITY_II{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input First Number");
+        int fnum = sc.nextInt();
+        System.out.println("Please Second Number");
+        int snum = sc.nextInt();
+
+        int sum = fnum+snum;
+        int dif = fnum-snum;
+        int prod = fnum*snum;
+        int quo = fnum/snum;
+
+
+
+		System.out.println("The Sum is: "+ sum);
+		System.out.println("The Difference is: "+ dif);
+		System.out.println("The Product is: "+ prod);
+		System.out.println("The Qoutient is: "+ quo);
+
+    }
+}
