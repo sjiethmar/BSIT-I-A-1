9900335e10c889080a32e216862ef6233b53ef14
kim louie N. olid <158005486+kimkim2003@users.noreply.github.com>
2024-02-12 10:30:27 +0800

    Add files via upload

---
+++ Activities/Activity IV/OLID_ACTIVITY_IV.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class OLID_ACTIVITY_IV {
+    
+    public static void main(String {}args){
+        Scanner input = new Scanner(System.in);
+        int color;
+        System.out.println("Pick a number (1.Red 2.Green 3.Black): ");
+        int num = input.nextInt();
+
+        switch(color) {
+            case 1:
+              System.out.println("1");
+              break;
+            case 2:
+              System.out.println("2");
+              break;
+            case 3:
+              System.out.println("3");  
+              default:
+              
+        }
+    }
+}
