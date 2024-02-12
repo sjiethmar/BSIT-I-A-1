885d8cd6d38056df4c4d20375187ce92c1974fda
JACK NEL REBIAS <156990141+JackNel395@users.noreply.github.com>
2024-02-12 10:41:52 +0800

    Update REBIAS_ACTIVITY_IV.java

--- Activities/Activity IV/REBIAS_ACTIVITY_IV.java
+++ Activities/Activity IV/REBIAS_ACTIVITY_IV.java
@@ -4,30 +4,25 @@ public class REBIAS_ACTIVITY_IV {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(" Enter color");
-        String robot = sc.nextln();
-        switch (
-            case case "red";
-            System.err.println("1");
-            break;
-            case "green";
-            System.out.println("2");
-            break;
-            case "black";
-            System.out.println("3");
-            default:
-            System.out.println("nothing");
 
-        ) {
-            case value:
-                
+        String robot = sc.nextln();
+
+        switch (robot) {
+            case "Red":
+            System.out.println(" Number 1");
                 break;
-        
-            default:
+                case "Green":
+                System.out.println(" Number 2");
                 break;
+                case "Black":
+                System.out.println("Number 3");
+                break;
+                default:
+                System.out.println("Nothing");
+
         }
 
+       
         }
 
     }
-
-}
