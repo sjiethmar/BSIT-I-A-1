059e21860fd35908e7fdce1563ba01c2e750e9a0
John Lez Tayona <156988440+johntayona@users.noreply.github.com>
2024-02-12 10:29:24 +0800

    Add files via upload

---
+++ Activities/Activity IV/[TAYONA_ACTIVITY_IV].java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TAYONA_ACTIVITY_IV{
+
+	public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+			 int boxNumber;
+
+	        System.out.println("Please Input Color: ");
+	        String color = scanner.nextLine();
+
+
+	        switch (color){
+	            case "red":
+	                boxNumber = 1;
+	                break;
+	            case "green":
+	                boxNumber = 2;
+	                break;
+	            case "black":
+	                boxNumber = 3;
+	                break;
+	            default:
+	                boxNumber = -1;
+	                break;
+
+	        }
+	        if (boxNumber != -1){
+	            System.out.println(boxNumber);
+	        }else{
+	            System.out.println("Unknown color");
+	        }
+
+	    }
+}
