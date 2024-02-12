8210917eda658520728dc112e906408dd34cf0e6
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-02-12 10:32:09 +0800

    Add files via upload

---
+++ Activities/Activity IV/IPILI_IRISH_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IPILI_IRISH_ACTIVITY_IV{
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter color:");
+    String robot = sc.nextLine();
+
+    Switch(robot){
+        case "RED":
+        System.out.println("Number 1");
+        break;
+        case "GREEN":
+        System.out.println("number 2");
+        break;
+        case "BLACK":
+        System.out.println("Number 3");
+        default:
+        System.out.print("Nothing");
+    }
+
+
+    }
+    
+}
+}
