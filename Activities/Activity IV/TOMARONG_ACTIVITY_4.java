e3e628ceec52f4d7792ce21be2fdee60c11607fe
Laiza Mae Tomarong <156989768+Laizamae17@users.noreply.github.com>
2024-02-12 10:26:31 +0800

    Add files via upload

---
+++ Activities/Activity IV/TOMARONG_ACTIVITY_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TOMARONG_ACTIVITY_4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int box =2;
+        switch(robot){
+            box 1;
+            System.out.println("Color Red");
+            break;
+            box 2;
+            System.out.println("Color Green");
+            break;
+            box 3;
+            System.out.println("Color Black");
+            break;
+        }
+    }
+}
+    
+
