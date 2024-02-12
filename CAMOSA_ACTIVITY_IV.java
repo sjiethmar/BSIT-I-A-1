71688b94eb80a8157c4b037a5e97d0fe6b1d3f66
Hazel Camosa <157287321+HazelCamosa13@users.noreply.github.com>
2024-02-12 10:24:58 +0800

    Add files via upload

---
+++ CAMOSA_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CAMOSA_ACTIVITY_IV{
+  public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter color");
+    String robot = sc.nextLine();
+    switch(box){
+        case red:
+         System.out.println("Number 1");
+        break;
+         case green:
+         System.out.println("Number 2");
+        break;
+         case black:
+         System.out.println("Number 3");
+        break;
+      default:
+      System.out.println("Nothing");
+
+    }
+  }
+}
