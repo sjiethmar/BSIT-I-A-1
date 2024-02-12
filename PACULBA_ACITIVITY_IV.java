337545af9a35c6ef694ee6463f4b8caf3ea40192
Paculba,Angelyn <158006374+paculbaange@users.noreply.github.com>
2024-02-12 10:26:54 +0800

    PACULBA_ACITIVITY_IV.java

---
+++ PACULBA_ACITIVITY_IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+    public class PACULBA_ACTIVITY_IIII{
+        public static void main(String []args){
+            Scanner sc = new Scanner(System.in);
+            System.out.print( "Enter color ");
+            String robot = sc.nextLine();
+
+            switch(robot){
+                case "Red":
+                System.out.println( "Number 1");
+                break;
+                case "Green":
+                System.out.println( "Number 2");
+                break;
+                case "Blue":
+                System.out.println( "Number 3");
+                break;
+                default:
+                System.out.print( "Nothing");
+
+            }
+        }
+
+    }
