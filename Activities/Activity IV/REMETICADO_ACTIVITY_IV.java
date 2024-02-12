34e809420ab8a86ec79aff3967e6ae23fa95b00b
Jedryl Remeticado <158005371+jeduuujed@users.noreply.github.com>
2024-02-12 10:28:55 +0800

    Add files via upload

---
+++ Activities/Activity IV/REMETICADO_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class REMETICADO_ACTIVITY_II{
+
+    public static void main (String[]args ){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Red no.1");
+        System.out.print("Orange no.2");
+        System.out.print("Green no.2");
+        String box = keyboard.nextLine();
+
+        switch (box){
+
+            case "Red":
+            System.out.println(x:"1");
+            break;
+            case "Orange":
+            System.out.println(x:"2");
+            break;
+            case "Green":
+            System.out.println("3");
+            break;
+        }
+
+        
+        
+    }
+}
