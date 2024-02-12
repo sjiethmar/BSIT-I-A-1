c4946f154fb0e1661fd4ac252745ff5cee2a1621
JACK NEL REBIAS <156990141+JackNel395@users.noreply.github.com>
2024-02-12 10:31:50 +0800

    Add files via upload

---
+++ Activities/Activity IV/REBIAS_ACTIVITY_IV.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class REBIAS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter color");
+        String robot = sc.nextln();
+        switch (
+            case case "red";
+            System.err.println("1");
+            break;
+            case "green";
+            System.out.println("2");
+            break;
+            case "black";
+            System.out.println("3");
+            default:
+            System.out.println("nothing");
+
+        ) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+        }
+
+    }
+
+}
