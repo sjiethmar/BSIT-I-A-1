29e28cabea568fd982ca2f45efd60990e33f2d1d
Crystal Jeanne Ortega <158002778+crystaljeanne@users.noreply.github.com>
2024-02-12 10:27:09 +0800

    Add files via upload

---
+++ Activities/Activity IV/ORTEGA_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ORTEGA_ACTIVITY_IV {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter color: ");
+        String color = sc.nextLine();
+        int red = 1, green = 2, black = 3;
+
+
+        switch(color) {
+            case "RED":
+            System.out.println("1");
+            break;
+            case "GREEN":
+            System.out.println("2");
+            break;
+            case "BLACK":
+            System.out.println("3");
+            break;
+        }
+
+
+        
+
+    }
+
+    }
+    
+
