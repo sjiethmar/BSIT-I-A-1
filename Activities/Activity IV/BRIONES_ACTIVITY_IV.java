8a0c3f0d0000edc129c010df422ad2520eca3e47
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-02-12 10:26:42 +0800

    Add files via upload

---
+++ Activities/Activity IV/BRIONES_ACTIVITY_IV.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BRIONES_ACTIVITY_IV{
+
+    public static void main (String []args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print(s:"Enter Color " (red, green, black));
+        String robot = sc.nextline();
+
+        Switch(robot){
+            case "Red":
+                System.out.println("Number 1");
+            break;
+            case "Green":
+                System.out.println("Number 2");
+            break;
+            case "Black":
+                System.out.println("Number 3");
+            break;
+            }
+            if(boxNumber !=-1){
+                System.out.println(bocNumber):
+            }else{
+                System.out.println("Unknown color"):
+                
+            }
+
+    }
+}
+        
