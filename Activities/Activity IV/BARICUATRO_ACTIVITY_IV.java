6e00bd42f573bef8ff76d7141c2db52a9b96c050
Ethel Rose U. Baricuatro <158002930+itilrozz@users.noreply.github.com>
2024-02-12 10:19:44 +0800

    Add files via upload

---
+++ Activities/Activity IV/BARICUATRO_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class BARICUATRO_ACTIVITY_IV {
+    public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    
+
+    System.out.print("Please enter a color(red, green or black): ");
+    String color =  sc.nextLine();
+
+        switch (color) {
+            case "red":
+                System.out.println("1");
+            break;
+            case "green":
+                System.out.println("2");
+            break;
+            case "black":
+                System.out.println("3");
+            break; 
+        }
+    }
+}
