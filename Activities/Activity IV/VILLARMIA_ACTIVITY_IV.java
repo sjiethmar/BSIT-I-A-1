846b3abd117a48a04e9623d21be32a5312050337
Gian Gadiano Villarmia <157211284+GianVillarmia18@users.noreply.github.com>
2024-02-12 10:31:33 +0800

    Add files via upload

---
+++ Activities/Activity IV/VILLARMIA_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+ public class VILLARMIA_ACTIVITY_IV{
+
+    public static void main (String []args){
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Enter Color");
+        String Robot = sc.nextLine();
+ 
+    switch(robot){
+        case "Red"
+        System.out.println("Number 1");
+        break;
+        case "Green"
+        System.out.println("Number 2");
+        break;
+        case "Black"
+        System.out.println("Number 3");
+        break; 
+        default:
+        System.out.println("Nothing");
+    }
+
+  }
+
+ }
+
+ 
