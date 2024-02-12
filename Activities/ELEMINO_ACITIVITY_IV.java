03677693fcf6154d357997a6bf5defa1edfab211
KurtElemino <158981359+KurtElemino@users.noreply.github.com>
2024-02-12 10:25:01 +0800

    ELEMINO_ACITIVITY_IV

---
+++ Activities/ELEMINO_ACITIVITY_IV.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+    public class ELEMINO_ACTIVITY_IV {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+
+        int box = sc.nextLine();
+        switch(box) {
+            case red:
+                System.out.println("1");
+                break;
+            case green:
+                System.out.println("2");
+                break;
+            case black:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println(x:"4");
+                break;
+        }
+        }
+        }
