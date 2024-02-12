9e3300543c635e7dd1fcd95a0922f1f7804d09cb
Christiancomeros24 <156991702+Christiancomeros24@users.noreply.github.com>
2024-02-12 10:36:04 +0800

    Add files via upload

---
+++ Activities/Activity I/COMEROS_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+public class COMEROS_ACTIVITY_1{
+    public static void main(String []args){
+
+        int a = 10, b = 10, c = 5;
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("Product: "+prod);
+        System.out.println("Quotient: "+quot);
+        
+        
+
+
+    }
+}
