89fce5d925c48f191c13e80eba296e03ad52682d
John franz O. Caliguid <158005719+johnfrank7@users.noreply.github.com>
2024-02-19 11:09:13 +0800

    Add files via upload

---
+++ Activities/Activity VI/CALIGUID_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CALIGUID_ACTIVITY_VI{
+	public static void main(String[]args){
+		Scanner input= new Scanner(System.in);
+		int x;
+
+		System.out.print("Input Second: ");
+		x = input.nextInt();
+
+		while(x>=0){
+			System.out.println(x);
+			x--;
+			}
+
+
+
+ }
+}
