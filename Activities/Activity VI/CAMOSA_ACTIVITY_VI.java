92f001b32f476b0df4c3bc311e22e2508e9614e8
Hazel Camosa <157287321+HazelCamosa13@users.noreply.github.com>
2024-02-19 11:10:59 +0800

    CAMOSA_ACTIVITY_VI

---
+++ Activities/Activity VI/CAMOSA_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CAMOSA_ACTIVITY_VI{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter an Integer:");
+        int sec = sc.nextInt();
+
+
+        while(sec >= 0){
+            System.out.println(sec);
+            sec--;
+        }
+    }
+}
