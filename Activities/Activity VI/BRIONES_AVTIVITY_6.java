8e5a2de42944f8e43d77d4f5ea6c27c2d4a66969
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-02-19 11:10:27 +0800

    Add files via upload

---
+++ Activities/Activity VI/BRIONES_AVTIVITY_6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class BRIONES_ACTIVITY_6{
+        public static void main(String []args){
+            Scanner sc = new Scanner(System.in);
+
+
+
+            System.out.print("Enter nymber seconds" );
+            int N = sc.nextInt();
+
+
+
+			while (N>=0){
+			System.out.println(N);
+			N--;
+
+
+			}
+
+          }
+    }
