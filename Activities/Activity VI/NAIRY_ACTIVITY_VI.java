5af39c10c366a60dc9dce92a3befe8559c70cae1
jaybienairy <158005907+jaybienairy@users.noreply.github.com>
2024-02-19 11:15:20 +0800

    Add files via upload

--- Activities/Activity VI/NAIRY_ACTIVITY_VI.java
+++ Activities/Activity VI/NAIRY_ACTIVITY_VI.java
@@ -1,15 +1,15 @@
 import  java.util.Scanner;
-public class NAIRY_ACTIVITY_VI {
+  public class NAIRY_ACTIVITY_VI {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        int sum = 0;
-        
-        for (int i = 1; i <= N; i++){
-            sum += i;
-        }
-        System.out.println(sum);
-    }
-
-}
+       Scanner sc = new Scanner(System.in);
+   
+	  
+	  			System.out.print("Enter a Number: ");
+	  			int time= sc.nextInt();
+	   while (time>=0){
+	  			System.out.println(time);
+	  		 time--;
+	  		}
+	  	}
+	}
+      
