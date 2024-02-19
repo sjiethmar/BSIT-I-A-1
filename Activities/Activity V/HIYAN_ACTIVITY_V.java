e324dbb066923623a36e71e6b4c462a813c7a565
Jenny Luo Cano Hiyan <158005724+jennyluo3@users.noreply.github.com>
2024-02-19 11:10:12 +0800

    Add files via upload

---
+++ Activities/Activity V/HIYAN_ACTIVITY_V.java
@@ -0,0 +1,23 @@
+import  java.util.Scanner;
+
+public class HIYAN_ACTIVITY_V {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Age: ");
+        int age = sc.nextInt();
+
+
+
+        if (age >= 0 && age <= 11){
+            System.out.println("Child");
+        } else if (age >= 12 && age <= 17){
+            System.out.println("Teen");
+        } else if (age >= 18 && age <= 64){
+            System.out.println("Adult");
+        } else {
+            System.out.println("Senior");
+        }
+
+    }
+}
+
