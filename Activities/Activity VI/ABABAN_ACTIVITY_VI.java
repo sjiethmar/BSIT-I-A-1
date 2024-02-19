9d1a401f92ce27494985e1d6dcb3ebb8959e3cfd
Kyle Marion N. Ababan <158006248+kyleababan@users.noreply.github.com>
2024-02-19 11:11:10 +0800

    Add files via upload

---
+++ Activities/Activity VI/ABABAN_ACTIVITY_VI.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+	public class ABABAN_ACTIVITY_VI{
+		public static void main (String []args){
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Enter a Number: ");
+			int time= sc.nextInt();
+ while (time>=0){
+			System.out.println(time);
+		 time--;
+		}
+	}
+	}
