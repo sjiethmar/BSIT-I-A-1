b0c3b96e112ff5292b41a48179620544f9be0b38
kim louie N. olid <158005486+kimkim2003@users.noreply.github.com>
2024-02-19 11:14:47 +0800

    Add files via upload

---
+++ Activities/Activity VI/[OLID_ACTIVITY_VI].java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class [OLID_ACTIVITY_VI]
+
+    public static void main(String[]args{
+        Scanner sc = new Scanner (System.in); 
+
+    System.out.println("Enter a Number of a second");
+    int time = input.nextln();
+
+    While (time) >= 0 ){
+        System.out.println(Time);
+
+        
+    }
