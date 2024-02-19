7a7feff055937deedb1464999daedccb6d158dd1
Eldrie Yancy C. Icoy <158005676+EldrieYancyIcoy@users.noreply.github.com>
2024-02-19 11:11:42 +0800

    Add files via upload

---
+++ Activities/Activity VI/ICOY_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ICOY_ACTIVITY_VI{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+
+System.out.print("Input Seconds: ");
+int N = sc.nextInt();
+
+while(N>=0){
+    System.out.println(N);
+    N--;
+
+        }
+	}
+}
