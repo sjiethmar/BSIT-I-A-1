6355f11fffe950fdf0a30d1b45a6e08cc850de10
Joward Remeticado <157957512+Wardo30@users.noreply.github.com>
2024-02-19 11:08:09 +0800

    Add files via upload

--- Activities/Activity IV/REMETICADO_ACTIVITY_IV.java
+++ Activities/Activity IV/REMETICADO_ACTIVITY_IV.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
-public class REMETICADO_ACTIVITY_II{
+public class REMETICADO_JOWARD_Activity_V{
+    public static void main (String []args){
 
-    public static void main (String[]args ){
+        int age;  
         Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter Age: ");
 
-        System.out.print("Red no.1");
-        System.out.print("Orange no.2");
-        System.out.print("Green no.2");
-        String box = keyboard.nextLine();
+        if (age = 0 || age > 12){
+            System.out.println("Child");
+        
+        }else if (age = 12 || age > 18);
+            System.out.println("Child");
 
-        switch (box){
+            else if (age = 18 || age > 65);
+            System.out.println("Adult"));
 
-            case "Red":
-            System.out.println(x:"1");
-            break;
-            case "Orange":
-            System.out.println(x:"2");
-            break;
-            case "Green":
-            System.out.println(x:"3");
-            break;
+        
+        
+  
+
+      
         }
 
-        
-        
     }
 }
