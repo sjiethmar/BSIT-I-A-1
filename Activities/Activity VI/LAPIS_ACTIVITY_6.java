a5f7e45c874cea56e1b23504515df4016449c0f2
Jaild Katte C. Lapis <158005981+jaildkatte@users.noreply.github.com>
2024-02-19 11:09:14 +0800

    Add files via upload

---
+++ Activities/Activity VI/LAPIS_ACTIVITY_6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+    public class LAPIS_ACTIVITY_6 {
+
+        public static void main(String[] args){
+            Scanner sc = new scanner(System.In);
+            System.out.println("Enter number of time:");
+            int num = sc.nextInt();
+
+
+            While (num >=0){
+                System.out.println(num);
+                num--;
+            } 
+        }
+         
+        
+    }
