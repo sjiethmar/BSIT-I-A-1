bdb9c7a3a8fb2106aeb8b303537e7a6a9ad1acbb
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-02-19 11:10:30 +0800

    Add files via upload

---
+++ Activities/Activity VI/BUHIA_ACTIVITY_VI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class BUHIA_ACTIVITY_VI{
+        public static void main(String []args){
+            Scanner sc = new Scanner(System.in);
+            int sec = 100; 
+            System.out.println("Enter the Countdown: ");
+            int sec = sc.nextInt();
+
+            while(num >=0) {
+                System.out.println(sec);
+                sec--;
+            }
+        }
+    }
