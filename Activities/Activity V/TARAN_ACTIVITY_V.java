6296b4c8ffb676cd01348ea06237b36dc5d9bd82
Earl Joseph D. Taran <156991493+Earljoseph21@users.noreply.github.com>
2024-02-19 11:08:44 +0800

    Create TARAN_ACTIVITY_V.java

---
+++ Activities/Activity V/TARAN_ACTIVITY_V.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TARAN_ACTIVITY_V {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int age;
+
+        System.out.print("Please enter your age: ");
+        age =  sc.nextInt();
+       
+            if (age>=0){
+                if (age>=0 && age<=11){
+                    System.out.println("Child");
+                } else if (age>=12 && age<=17){
+                    System.out.println("Teen");
+                } else if (age>= 18 && age<=64){
+                    System.out.println("Adult");
+                } else {
+                    System.out.println("Senior");
+                }
+            } else
+            System.out.println("error");
+    }
+            
+}
