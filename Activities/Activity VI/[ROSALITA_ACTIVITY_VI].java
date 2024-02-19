4c2a9a58bdd2ef01a875d8dd909b930ea0581d45
A Jay R. Rosalita <157581445+ajayrosalita5@users.noreply.github.com>
2024-02-19 11:11:12 +0800

    Add files via upload

---
+++ Activities/Activity VI/[ROSALITA_ACTIVITY_VI].java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ROSALITA_ACTIVITY_VI{
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        
+        Sytem.out.println("Enter the number of seconds");
+        int time  = input.nextInt();
+
+        while (time >= 0 ){
+            System.out.println(time);
+            time--;
+        }
+        
+        
+    }
+
+    
+}
+   
