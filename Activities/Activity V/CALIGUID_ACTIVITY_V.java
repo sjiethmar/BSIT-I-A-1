867be37d9be42eeedfa7ef4b8409bee44e824864
John franz O. Caliguid <158005719+johnfrank7@users.noreply.github.com>
2024-02-19 11:08:20 +0800

    Add files via upload

---
+++ Activities/Activity V/CALIGUID_ACTIVITY_V.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CALIGUID_ACTIVITY_V{
+	public static void main(String[]args){
+		Scanner input= new Scanner(System.in);
+		int age;
+
+		System.out.print("Enter age: ");
+		age = input.nextInt();
+
+		if(age>=0 && age<=11){
+			System.out.println("Child");
+		}else if(age>=12 && age<=17){
+			System.out.println("Teen");
+		}else if(age>=18 && age<=64){
+			System.out.println("Adult");
+		}else
+			System.out.println("Senior");
+	}
+}
