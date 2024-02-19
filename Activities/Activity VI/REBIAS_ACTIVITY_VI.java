55d0bb8b2a455e52c23c03842f6340a77cf33747
JACK NEL REBIAS <156990141+JackNel395@users.noreply.github.com>
2024-02-19 11:10:00 +0800

    Add files via upload

---
+++ Activities/Activity VI/REBIAS_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class REBIAS_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of seconds: ");
+        int seconds = scanner.nextInt();
+
+        while (seconds) {    
+        } (int i = seconds; i >= 0; i--) {
+            System.out.println("Seconds remaining: " + i)
+        }
+        System.out.println("Countdown complete!");
+        scanner.close();
+    }
+}
+
+
+
+    
