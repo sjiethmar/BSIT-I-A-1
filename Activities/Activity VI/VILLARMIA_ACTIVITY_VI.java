b3b6e82509b0166562b9ea511c1748432c795125
Gian Gadiano Villarmia <157211284+GianVillarmia18@users.noreply.github.com>
2024-02-19 11:09:54 +0800

    Add files via upload

---
+++ Activities/Activity VI/VILLARMIA_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ public class VILLARMIA_ACTIVITY_VI {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of seconds; ");
+        int seconds = sc.nextInt();
+        
+        while (seconds){
+        }(int i= seconds; i >= 0; i--)}
+            System.out.println("Seconds remaining: + i");
+        }
+        System.out.println("Countdown completed!");
+        scanner.close();
+       
+
+    }
+    
+}
