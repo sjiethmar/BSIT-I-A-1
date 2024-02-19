1594b4c06b181dc13bd2f9143f789345ccfcff37
Sarcon Chane B <158005300+chanesarcon@users.noreply.github.com>
2024-02-18 19:14:36 -0800

    Create SARCON_ACTIVITY_VI.java

---
+++ Activities/Activity VI/SARCON_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class SARCON_ACTIVITY_VI { 
+   
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter number of time:");
+            int num = sc.nextInt();
+    
+           
+            while(num >=0){ 
+                System.out.println(num);
+                num--;
+        }
+        
+    
+    }
+}
+ 
