62101d3fd270991460e57684fa3e4069f3c15d6d
Rhea Mae L. Jayme <157574770+rheajayme@users.noreply.github.com>
2024-02-19 11:08:04 +0800

    Add files via upload

---
+++ Activities/Activity V/JAYME_ACTIVITY_V.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JAYME_ACTIVITY_V {
+  public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+    System.out.print("Enter Age: ");
+    Integer age = sc.nextInt();
+
+    (age >=0 && age <=11);
+    (age >=12 && age <=17);
+    (age >=18 && age <=64);
+    System.out.println("Child");
+    System.out.println("Teen");
+    System.out.println("Adult");
+
+}
+}
