b3cff24811e084337a183453ce41612d065097d9
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-02-19 11:09:20 +0800

    Add files via upload

---
+++ Activities/Activity VI/IPILI_IRISH_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IPILI_IRISH_ACTIVITY_VI {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a time:");
+        int time = sc.nextInt();
+
+        while(time <= 5 ){
+            System.out.println(time);
+            time ++;
+        }
+
+        
+    }
+    
+}
